4e1aa8f34860ae356209eb1d5b932cb6d98290eb
vice76 <arpityadavaj1999@gmail.com>
2021-07-01 09:25:10 +0530

    added lower common ancestor of a binary tree

---
+++ June LeetCoding Challenge/Lower_Common_Ancestor_Of_A_Binary_tree.java
@@ -0,0 +1,18 @@
+
+import java.util.*;
+import java.io.*;
+
+class Solution {
+        
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root==p || root==q || root==null)
+            return root;
+        TreeNode left=lowestCommonAncestor(root.left,p,q);
+        TreeNode right=lowestCommonAncestor(root.right,p,q);
+        if(left==null && right==null)
+            return null;
+        if(left!=null && right!=null)
+            return root;
+        return (left==null)?(right==null)?left:right:left;
+    }
+}
